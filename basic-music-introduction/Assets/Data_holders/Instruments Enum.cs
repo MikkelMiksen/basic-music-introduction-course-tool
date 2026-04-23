3d539efb3ef3c703bfcdfafcf8908d98a99eb41a
MikkelMiksen <mikkel.juel123@gmail.com>
2026-04-23 16:43:47 +0200

    fixing..

---
+++ basic-music-introduction/Assets/Data_holders/Instruments Enum.cs
@@ -0,0 +1,11 @@
+
+namespace Data_holders.instruments
+{
+    public enum Instruments
+    {
+        Closed_HiHat,
+        Open_HiHat,
+        Snare,
+        Kick,
+    }
+}
--- basic-music-introduction/Assets/Note_Manager_MJ/Accompanying code/Note Manager.cs
+++ basic-music-introduction/Assets/Note_Manager_MJ/Accompanying code/Note Manager.cs
@@ -1,10 +1,36 @@
+using System.Collections.Generic;
+using Data_holders.instruments;
 using UnityEngine;
 
 public class NoteManager : MonoBehaviour
 {
+    private (Instruments, float) CH(float duration)
+    {
+        return (Instruments.Closed_HiHat, duration);
+    }
+    
+    private (Instruments, float) OH(float duration)
+    {
+        return (Instruments.Open_HiHat, duration);
+    }
+    
+    private (Instruments, float) Kick(float duration)
+    {
+        return (Instruments.Kick, duration);
+    }
+
+    private (Instruments, float) Snare(float duration)
+    {
+        return (Instruments.Snare, duration);
+    }
+
+    private List<(Instruments, float)> notes = new();
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+        //here goes the logic for filling in the duration in the list baseed on the color of the scan input
         
     }
 
